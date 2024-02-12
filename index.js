afebceaef9fda33201a8d7c12aa15f5decb53cb7
mauricio <mauricio.chaves.chaves@est.una.ac.cr>
2024-02-12 17:56:59 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () { //que va a pasar si se pulsa el boton si
+    alert('Jaja que cringe el vato puso  uwu')
+});
+
+const noBtn = document.querySelector('#noBtn');
+let contador = 0;
+
+noBtn.addEventListener('mouseover', function () { //que va a pasar si se PONE el cursor sobre el boton no
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+
+    contador++;
+
+    if (contador === 10) {
+        console.log("estoy contando: "+ contador);
+        mostrarAlertaCont();
+        contador == 0;
+    }
+})
+
+//Pruebas----------------------------------------------
+function alertaPruebaClick() {
+    return Swal.fire({
+        title: 'Éxito',
+        text: 'a ver si cierto...',
+        icon: 'success',
+        timer: 2000,
+        showConfirmButton: false
+    })
+        .then(() => {
+            window.location.href = "https://www.ubereats.com/cr";
+        });
+}
+
+function alertaCancelar() {
+    Swal.fire(
+        'Cancelado',
+        'Cerraste el modal',
+        'error'
+    )
+}
+
+function mostrarAlertaCont() {
+    Swal.fire({
+        title: 'A ver a ver ',
+        text: 'Por que no me compras unas papas',
+        icon: 'has feliz a este pobre niño',
+        confirmButtonText: 'Si,soy un amor',
+        cancelButtonText: 'No, soy uan carepicha'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            alertaPruebaClick()
+        } else {
+            alertaCancelar()
+        }
+    })
+}
